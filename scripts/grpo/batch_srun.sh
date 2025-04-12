4bbe7b99bc43e21840db40c8117155550a65d7f3
CMU <cmu@oumi-cmu-01.us-east5-a.c.lema-dev.internal>
2025-04-12 05:55:47 +0000

    changed compute_score

--- scripts/grpo/batch_srun.sh
+++ scripts/grpo/batch_srun.sh
@@ -1,19 +1,12 @@
 
 export CONTEXT_LENGTH=2048
-export BASE_MODEL=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
+export BASE_MODEL=Qwen/Qwen2.5-Math-1.5B
 
-
-# export DATA_DISTRIBUTION="train"
-# export EXPERIMENT_NAME="$DATA_DISTRIBUTION-$CONTEXT_LENGTH"
-# export EPOCHS=2
-
-# nohup bash /home/cmu/math-curriculum/scripts/grpo/grpo.sh > /home/cmu/math-curriculum/logs/$EXPERIMENT_NAME.log 2>&1
-
-
-for dist in "train-3-5" "train-1-5"; do
+for dist in "train-1-5" "train-3-5"; do
     export DATA_DISTRIBUTION=$dist
     export EXPERIMENT_NAME="$DATA_DISTRIBUTION-$CONTEXT_LENGTH"
     export EPOCHS=6
     
     nohup bash /home/cmu/math-curriculum/scripts/grpo/grpo.sh > /home/cmu/math-curriculum/logs/$EXPERIMENT_NAME.log 2>&1
 done
+
